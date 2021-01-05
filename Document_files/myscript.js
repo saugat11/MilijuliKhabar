092e8d4053ab1f6312278bea17999561a2e3da12
saugat11 <76836630+saugat11@users.noreply.github.com>
2021-01-05 14:11:17 +0545

    added milijulikhabar contents

---
+++ Document_files/myscript.js
@@ -0,0 +1,13 @@
+
+window.addEvent('load',function(){
+    document.getElementById('Script').innerHTML="Hey i came from the innerhtml";
+    
+  });
+  
+    
+    
+    function myfunction(){
+        document.getElementById("change").innerHTML="BYE!!"
+    }
+
+    
